ba1d506fdddf3e0efacf6afc04d7ba7c537bd104
MarianaAddeo <82729297+MarianaAddeo@users.noreply.github.com>
2024-01-12 03:31:03 -0300

    Add files via upload

---
+++ exe003t.py
@@ -0,0 +1,6 @@
+entr1= input('Qual é o seu nome?')
+entra2= int(input('Qual é a sua idade?'))
+valord=int(input('Qual é o valor disponível?'))
+saq= int(input('Qual é o valor que você quer sacar?'))
+conta= valord - saq
+print('O usuário é',entr1, 'e possui',entra2,'anos. E possui o valor',conta,'disponível no momento.')
